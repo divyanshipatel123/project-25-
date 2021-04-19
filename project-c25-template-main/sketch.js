33c10ab26c07a8dd6990f185905ed42a9dcabc6a
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2021-04-19 20:06:30 +0530

    ......................................................

--- project-c25-template-main/sketch.js
+++ project-c25-template-main/sketch.js
@@ -6,7 +6,7 @@ const Body = Matter.Body;
 const Render = Matter.Render;
 var dustbinObj,groundObject	
 var world;
-var paper;
+var paper1;
 
 
 function setup() {
@@ -19,7 +19,7 @@ function setup() {
 	
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-	paper = new Paper(400,100);
+	paper1 = new Paper(400,100);
 
 	Engine.run(engine);
   
@@ -40,7 +40,7 @@ function draw() {
 
 function keyPressed(){
    if(keyDown(UP_ARROW)){
-	Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-145})   
+	Matter.Body.applyForce(paper1.body,paper1.body.position,{x:130,y:-145})   
    }
 
 }
